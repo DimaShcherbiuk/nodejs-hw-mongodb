44c51a0cb5cf56d3419cc63a6fa14231b6d76622
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-08-18 17:39:07 +0300

    add swagger

--- src/server.js
+++ src/server.js
@@ -37,6 +37,11 @@ export const setupServer = () => {
 
   app.use(errorHandler);
 
+  const swaggerUi = require('swagger-ui-express');
+  const swaggerDocument = require('./docs/swagger.json');
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
